3a666b5ef45a3a3954a4cb468461e6ce5bc5a7e5
EmmanuelAgbaga <dmaduss12@gmail.com>
2023-08-14 12:07:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,13 +10,11 @@ int main(void)
         int p;
 
         for (n = 48; n <= 57; n++)
-        {
+{
                 putchar(n);
-        }
+}
         for (p = 97; p <= 102; p++)
-        {
-                putchar(p);
-        }
+                putchar(p);}
         putchar('\n');
         return (0);
 }
